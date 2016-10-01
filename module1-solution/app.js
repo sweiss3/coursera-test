254a4f5a799aabf19cdf0ec0307a938c251af877
Shahar Weiss <sweiss3@gmail.com>
2016-10-01 20:33:18 +0300

    Solving first exercise

---
+++ module1-solution/app.js
@@ -0,0 +1,34 @@
+(function () {
+'use strict';
+
+
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', LunchCheckController);
+
+LunchCheckController.$inject = ['$scope'];
+function LunchCheckController($scope) {
+    $scope.lunchList = "";
+    $scope.resultColor = "black";
+    $scope.resultBorderWidth = 0;
+
+    $scope.checkLunch = function () {
+        var splitLunch = $scope.lunchList.split(",").filter(function (val) {
+            return val.trim().length > 0;
+        });
+
+        $scope.resultBorderWidth = 1;
+        if (splitLunch.length === 0) {
+            $scope.lunchResult = "Please enter data first";
+            $scope.resultColor = "red";
+        } else {
+            $scope.resultColor = "green";
+            if (splitLunch.length > 3) {
+                $scope.lunchResult = "Too much!";
+            } else {
+                $scope.lunchResult = "Enjoy!";
+            }
+        }
+    }
+}
+
+})();
